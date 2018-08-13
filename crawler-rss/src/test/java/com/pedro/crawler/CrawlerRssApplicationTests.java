e14bf865c5ec7a7bad593546ac4d0139062c98f9
Pedro <Pedro@192.168.1.104>
2018-08-12 21:04:08 -0300

    Adiciona validação de usuário logado.

--- crawler-rss/src/test/java/com/pedro/crawler/CrawlerRssApplicationTests.java
+++ crawler-rss/src/test/java/com/pedro/crawler/CrawlerRssApplicationTests.java
@@ -1,10 +1,9 @@
 package com.pedro.crawler;
 
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
-import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,14 +11,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
-
-import com.pedro.crawler.model.FeedItem;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CrawlerRssApplication.class)
@@ -31,17 +26,8 @@ public class CrawlerRssApplicationTests {
 	
 	private MockMvc mockMvc;
 	
-	private HttpMessageConverter<FeedItem> mappingJackson2HttpMessageConverter;
-	
 	@Autowired
 	private WebApplicationContext context;
-	
-	protected String json(FeedItem feedItem) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        mappingJackson2HttpMessageConverter.write(
-        		feedItem, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
-    }
 	
 	@Before
     public void setup() throws Exception {
@@ -53,6 +39,16 @@ public class CrawlerRssApplicationTests {
 		mockMvc.perform(get("/showItems")
                 .contentType(contentType))
                 .andExpect(status().isOk());
+	}
+	
+	@Test
+	public void showItemsFromMalformedUrl() throws Exception {
+		String response = mockMvc.perform(get("/showItems?url=s")
+                .contentType(contentType))
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+		assertTrue(response.equals("[]"));
 	}
 	
 }
